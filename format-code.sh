c87b63b48b16d0dc848e55aab3e21f13445b155b
Tyler Veness <calcmogul@gmail.com>
2015-02-02 02:19:49 -0800

    formatting: fixed incorrect conditionals in file finding heuristics

--- format-code.sh
+++ format-code.sh
@@ -11,8 +11,8 @@ args() {
 }
 
 format() {
-  find $PWD -not \( $i -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune \) ! -type d -name \*.cpp -o -name \*.hpp -o -name \*.inl | uncrustify -c uncrustify.cfg -F - -l CPP --replace --no-backup
-  find $PWD -not \( $i -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune \) ! -type d -name \*.c -o -name \*.h | uncrustify -c uncrustify.cfg -F - -l C --replace --no-backup
+  find $PWD ! -type d -not \( -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune \) -a \( -name \*.cpp -o -name \*.hpp -o -name \*.inl \) | uncrustify -c uncrustify.cfg -F - -l CPP --replace --no-backup
+  find $PWD ! -type d -not \( -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune \) -a \( -name \*.c -o -name \*.h \) | uncrustify -c uncrustify.cfg -F - -l C --replace --no-backup
 }
 
 if [ $# = 0 ] ; then
