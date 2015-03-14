c706559a943976d813aa191cbbdf89d98b8f0016
Tyler Veness <calcmogul@gmail.com>
2015-03-14 09:44:19 -0700

    Simplified send-robot-settings.txt

--- send-robot-settings.sh
+++ send-robot-settings.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 HOST='10.35.12.2'
-USER='anonymous'
+USER='lvuser'
 PASSWD=''
 
 # Add file names here to send them
@@ -8,24 +8,9 @@ FILESRC=('RobotSettings.txt' 'GUISettings.txt')
 PATHDEST=('/home/lvuser' '/home/lvuser')
 FILEDEST=('RobotSettings.txt' 'GUISettings.txt')
 
-# script: login
-echo "quote USER $USER
-quote PASS $PASSWD" > /tmp/ftp.$$
-
 # script: send files in list
 for (( i=0 ; i < ${#FILESRC[@]} ; i++ ));
 do
-    echo "cd ${PATHDEST[$i]}" >> /tmp/ftp.$$
-    echo "put ${FILESRC[$i]} ${FILEDEST[$i]}" >> /tmp/ftp.$$
+    scp ${FILEDEST[$i]} $USER@$HOST:${PATHDEST[$i]}
 done
-
-# script: logout of FTP server
-echo "quit" >> /tmp/ftp.$$
-
-# execute script
-ftp -ivn $HOST < /tmp/ftp.$$
-
-# delete script and exit
-rm /tmp/ftp.$$
-exit 0
 
